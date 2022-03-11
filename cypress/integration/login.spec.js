3339bded8daa3f80f5e40182980053876fd4dc15
jadwro <jadwro@gmail.com>
2022-03-11 17:22:00 +0100

    11.03

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -20,7 +20,7 @@ describe("Testing of login page", () => {
         LoginPage.enterPswd(data.correct.pswd);
         LoginPage.elements.validationDiv().should('not.exist');
         LoginPage.clickLogin();
-        cy.url().should('contain', 'users');
+        cy.url().should('contain', 'customers');
     });
 
     it("Logging in with incorrect credentials", () => {
@@ -67,5 +67,55 @@ describe("Testing of login page", () => {
         LoginPage.elements.forgotPopup()
             .should('be.visible')
             .should('contain', 'E-mail z linkiem do zmiany hasła został wysłany');
+    });
+
+    it("Logging in with correct credentials", () => {     
+      cy.intercept('GET', '**/api/systemManagement/Dictionaries/Integrators', (req, res) => {
+        req.headers['x-custom-header'] = 'added by cy.intercept'
+        req.statusCode = 400
+      }).as('integrators');
+      cy.intercept('POST', 'https://apidev.biz360.pl/api/systemManagement/Users', (req) => {
+        req.statusCode = 400
+      }).as('userSave');
+      LoginPage.enterEmail(data.correct.email);
+      LoginPage.enterPswd(data.correct.pswd);
+      LoginPage.elements.validationDiv().should('not.exist');
+      LoginPage.clickLogin();
+      cy.url().should('contain', 'users');
+      cy.wait('@integrators').then(() => {
+        cy.get('@integrators').its('request.headers').should('have.property', 'x-custom-header', 'added by cy.intercept')
+        cy.get('@integrators').its('response.statusCode').should('eq', 200);
+      })
+      cy.get('a').contains('Dodaj użytkownika').invoke('removeAttr', 'target').click();
+      cy.get('input[name=FirstName]').type('John');
+      cy.get('input[name=LastName]').type('Travolta');
+      cy.get('input[name=Email]').type('john@travolta.cooom');
+      cy.get('input[name=Phone]').type('474747477');
+      cy.get('#mui-component-select-CompanyId').click();
+      cy.get('ul[aria-labelledby="CompanyId"] > li').eq(0).click();
+      cy.get('button').contains('Zapisz').click();
+      cy.wait('@userSave').its('response.statusCode').should('eq', 200);
+    });
+
+    it.only("Logging in with correct credentials", () => {     
+      cy.intercept('GET', '**/api/systemManagement/Dictionaries/Integrators', (req) => {                        
+        const bodyy = req.body
+        // req.reply({
+        //   statusCode: 200,
+        //   // body: bodyy
+        // });
+        req.continue(res => {
+          res.statusCode = 500
+        })
+        
+      }).as('integrators');
+      LoginPage.enterEmail(data.correct.email);
+      LoginPage.enterPswd(data.correct.pswd);
+      LoginPage.elements.validationDiv().should('not.exist');
+      LoginPage.clickLogin();
+      cy.url().should('contain', 'users');
+      cy.wait('@integrators').then(() => {        
+        // cy.get('@integrators').its('response.statusCode').should('eq', 400);
+      })
     });
 });
